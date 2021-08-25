a698376455031c326361407cdb8dba15b25463be
Dave Thompsen <dave@dthompsen.com>
2021-08-25 14:40:57 -0400

    Remove old client-web UI - replaced by new web-app sub-project

--- client-web/src/components/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import { App } from '../App';
-
-it('renders app without error', async () => {
-  const comp = render( <App /> );
-  expect(comp.getByText(/Art Manager/));
-});
